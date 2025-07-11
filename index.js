ccac20202fd1912d916b045d8b3b9457e2e01a46
Jkites <99958936+Jkites@users.noreply.github.com>
2025-07-11 01:06:49 -0700

    added prod build

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
-
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :response-time ms :body'))
 app.use(express.json())
